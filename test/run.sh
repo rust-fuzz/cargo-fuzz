d18027f9df3923b928cadd7aed0c2e16eea73616
ner0x652 <cornel.punga@gmail.com>
2017-03-07 09:57:17 +0200

    Update travis CI script to run install and build commands

--- test/run.sh
+++ test/run.sh
@@ -1,8 +1,8 @@
-cd testcrate
-
+cd ..
+cargo install
+cd test/testcrate
+cargo build
 cargo-fuzz init
-
 sed -i 's/\/\/.*/testcrate\:\:test_func\(data\)\;/g' fuzz/fuzzers/fuzzer_script_1.rs
-
 cargo-fuzz run fuzzer_script_1
 
