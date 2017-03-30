15dc24a94d2017caa5693e793afd9241f7cc6794
Manish Goregaokar <manishsmail@gmail.com>
2017-03-29 18:33:12 -0700

    gcc 5

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,5 +4,13 @@ cd testcrate
 cargo build
 cargo-fuzz init
 sed -i 's/\/\/.*/testcrate\:\:test_func\(data\)\;/g' fuzz/fuzzers/fuzzer_script_1.rs
-cargo-fuzz run fuzzer_script_1
+
+if CC=gcc-5 CXX=g++-5 cargo-fuzz run fuzzer_script_1 -- -runs=1000; then
+    exit 100; # Should not succeed!
+else
+    :;
+fi
+
+# should be something there!
+ls fuzz/artifacts/fuzzer_script_1/crash-*
 
