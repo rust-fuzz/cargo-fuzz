07d3c75e096e4b25e56442918e06c01cf6c251f6
Atul Bhosale <atul1bhosale@gmail.com>
2019-10-28 12:40:12 +0530

    Format code using 'cargo fmt'

--- src/templates.rs
+++ src/templates.rs
@@ -1,6 +1,7 @@
 macro_rules! toml_template {
     ($name: expr) => {
-format_args!(r##"
+        format_args!(
+            r##"
 [package]
 name = "{0}-fuzz"
 version = "0.0.0"
@@ -19,38 +20,47 @@ git = "https://github.com/rust-fuzz/libfuzzer-sys.git"
 # Prevent this from interfering with workspaces
 [workspace]
 members = ["."]
-"##, $name)
-    }
+"##,
+            $name
+        )
+    };
 }
 
 macro_rules! toml_bin_template {
     ($name: expr) => {
-format_args!(r#"
+        format_args!(
+            r#"
 [[bin]]
 name = "{0}"
 path = "fuzz_targets/{0}.rs"
-"#, $name)
-    }
+"#,
+            $name
+        )
+    };
 }
 
 macro_rules! gitignore_template {
     () => {
-format_args!(r##"
+        format_args!(
+            r##"
 target
 corpus
 artifacts
-"##)
-    }
+"##
+        )
+    };
 }
 
 macro_rules! target_template {
     () => {
-format_args!(r##"#![no_main]
+        format_args!(
+            r##"#![no_main]
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {{
     // fuzzed code goes here
 }});
-"##)
-    }
+"##
+        )
+    };
 }
