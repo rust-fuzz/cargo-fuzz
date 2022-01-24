333e7112effe4267eebaddb55d45ed6c0de050ef
Lynn Kirby <lynn@arrak.is>
2022-01-24 16:44:18 -0500

    Add --no-trace-compares flag
    This will opt-out of the `-sanitizer-coverage-trace-compares` LLVM flag that
    cargo-fuzz builds with by default.

--- src/options.rs
+++ src/options.rs
@@ -133,6 +133,10 @@ pub struct BuildOptions {
     /// allows you to opt out of it.
     #[structopt(long)]
     pub no_cfg_fuzzing: bool,
+
+    #[structopt(long)]
+    /// Don't build with the `sanitizer-coverage-trace-compares` LLVM argument
+    pub no_trace_compares: bool,
 }
 
 impl stdfmt::Display for BuildOptions {
@@ -229,6 +233,7 @@ mod test {
             coverage: false,
             strip_dead_code: false,
             no_cfg_fuzzing: false,
+            no_trace_compares: false,
         };
 
         let opts = vec![
