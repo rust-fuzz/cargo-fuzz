d36b48b3ad277d6fc2e2f81f0448b6273931b533
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2022-03-30 17:43:58 +0200

    Rerun build.rs when needed; more comments

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 use std::env;
 
 fn main() {
+    // Cargo sets the host and target env vars for build scripts, but not crates:
+    // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts
+    // So we just re-export them to the crate code.
     println!("cargo:rustc-env=TARGET={}", env::var("TARGET").unwrap());
-    println!("cargo:rerun-if-changed=build.rs");
+    // By default Cargo only runs the build script when a file changes.
+    // This makes it re-run on target change
+     println!("cargo:rerun-if-changed-env=TARGET")
 }
